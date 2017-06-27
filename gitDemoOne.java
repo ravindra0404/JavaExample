68f57a6de6cc87388232911d8a21393da75bb2e7
ravindra0404 <ravindra.s@sonata-software.com>
2017-06-27 12:23:20 +0530

    commited

--- gitDemoOne.java
+++ gitDemoOne.java
@@ -1,8 +1,9 @@
-public class gitDemoOne{
+public class JavaGit{
 
 	public static void main(String[] args) {
 	System.out.println("This line is added newly");
 		System.out.println("Example for Git Demo");
+		System.out.println("I changed again can you knew it, OK");
 	}
 
 }
